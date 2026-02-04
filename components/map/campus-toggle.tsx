d3101ead50994debf1b554bde80799b758a1a864
Nolan Ganz <nolanganz1@gmail.com>
2026-02-03 21:56:29 -0500

    Created campus toggle button
    Styles still need to be done

---
+++ components/map/campus-toggle.tsx
@@ -0,0 +1,121 @@
+import { ThemedText } from "@/components/themed-text";
+import { Coordinate, CoordinateDelta, Region } from "@/types/mapTypes";
+import React, { useEffect, useMemo } from "react";
+import {
+  StyleSheet,
+} from "react-native";
+import SwitchSelector from "react-native-switch-selector";
+import MapView from "react-native-maps";
+
+interface Props {
+  initialRegion: Region;
+  mapRef: React.RefObject<MapView | null>;
+  onMount: (regionSetter: Function) => void;
+}
+
+const SGW = "SGW";
+const LOY = "LOY";
+
+const SGW_CENTER: Coordinate = {
+  latitude: 45.4957849,
+  longitude: -73.577225
+};
+
+const LOY_CENTER: Coordinate = {
+  latitude: 45.4578596,
+  longitude: -73.6395856,
+};
+
+const DEFAULT_ZOOM: CoordinateDelta = {
+  latitudeDelta: 0.01,
+  longitudeDelta: 0.01,
+}
+
+const SWITCH_STYLES = {
+  textColor: "#FFFFFF",
+  selectedColor: "#000000",
+  buttonColor: "#FFFFFF",
+  borderColor: "#000000",
+  backgroundColor: "#000000"
+}
+
+export default function CampusToggle({ initialRegion, mapRef, onMount }: Props) {
+
+  const [switchValue, setSwitchValue] = React.useState<string>(LOY);
+  const [viewRegion, setViewRegion] = React.useState<Region>(initialRegion);
+
+  useEffect(() => {
+    onMount(setViewRegion);
+  }, [onMount]);
+
+  useEffect(() => {
+    let swgDistance = Math.hypot(viewRegion.latitude - SGW_CENTER.latitude, viewRegion.longitude - SGW_CENTER.longitude);
+    let loyDistance = Math.hypot(viewRegion.latitude - LOY_CENTER.latitude, viewRegion.longitude - LOY_CENTER.longitude);
+    let newSwitchValue = swgDistance < loyDistance ? SGW : LOY;
+
+    if (newSwitchValue !== switchValue) {
+      setSwitchValue(newSwitchValue);
+    }
+    console.log("Current: " + switchValue);
+    console.log("Recalculated: " + newSwitchValue);
+  }, [viewRegion, switchValue]);
+
+  const focusCampusOnPress = (value: string) => {
+
+    setSwitchValue(value);
+
+    let newFocus: Coordinate;
+    if (value === SGW) {
+      newFocus = SGW_CENTER;
+    } else {
+      newFocus = LOY_CENTER;
+    }
+
+    mapRef.current?.animateToRegion({
+      ...newFocus,
+      ...DEFAULT_ZOOM,
+    });
+  };
+
+  return (
+    <SwitchSelector
+      initial={switchValue === LOY ? 0 : 1}
+      value={switchValue}
+      textColor={SWITCH_STYLES.textColor}
+      selectedColor={SWITCH_STYLES.selectedColor}
+      buttonColor={SWITCH_STYLES.buttonColor}
+      borderColor={SWITCH_STYLES.borderColor}
+      backgroundColor={SWITCH_STYLES.backgroundColor}
+      disableValueChangeOnPress={false}
+      hasPadding
+      options={[
+        { label: "Loyola", value: LOY }, 
+        { label: "SGW", value: SGW } 
+      ]}
+      testID="campus-toggle-selector"
+      accessibilityLabel="campus-toggle-selector"
+      onPress={focusCampusOnPress}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    width: "80%",
+    maxWidth: 300,
+    maxHeight: 200,
+    borderRadius: 30,
+    margin: "auto",
+    padding: 20,
+  },
+  text: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+  button: {
+    padding: 10,
+    alignItems: "center",
+  },
+});
