76971a6ba7c9e998ae61bd6c77831c5d8a7a93d6
Strange4 <83730323+Strange4@users.noreply.github.com>
2026-02-25 14:27:00 -0500

    [REFACTOR] - rename magic constants into enum

--- components/map/campus-toggle.tsx
+++ components/map/campus-toggle.tsx
@@ -1,9 +1,6 @@
 import { Coordinate, CoordinateDelta, Region } from "@/types/mapTypes";
 import React, { useEffect } from "react";
-import {
-  View,
-  StyleSheet,
-} from "react-native";
+import { View, StyleSheet } from "react-native";
 import SwitchSelector from "react-native-switch-selector";
 import MapView from "react-native-maps";
 import { useColorScheme } from "@/hooks/use-color-scheme";
@@ -14,12 +11,14 @@ interface Props {
   viewRegion: Region;
 }
 
-const LOY = 0;
-const SGW = 1;
+enum Campus {
+  Loyola = 0,
+  SGW = 1,
+}
 
 const SGW_CENTER: Coordinate = {
   latitude: 45.4957849,
-  longitude: -73.577225
+  longitude: -73.577225,
 };
 
 const LOY_CENTER: Coordinate = {
@@ -30,20 +29,32 @@ const LOY_CENTER: Coordinate = {
 const DEFAULT_ZOOM: CoordinateDelta = {
   latitudeDelta: 0.01,
   longitudeDelta: 0.01,
-}
+};
 
 export default function CampusToggle({ mapRef, viewRegion }: Readonly<Props>) {
   const colorScheme = useColorScheme();
   const [switchValue, setSwitchValue] = React.useState<number>(() => {
-    let sgwDistance = Math.hypot(viewRegion.latitude - SGW_CENTER.latitude, viewRegion.longitude - SGW_CENTER.longitude);
-    let loyDistance = Math.hypot(viewRegion.latitude - LOY_CENTER.latitude, viewRegion.longitude - LOY_CENTER.longitude);
-    return sgwDistance < loyDistance ? SGW : LOY;
+    let sgwDistance = Math.hypot(
+      viewRegion.latitude - SGW_CENTER.latitude,
+      viewRegion.longitude - SGW_CENTER.longitude,
+    );
+    let loyDistance = Math.hypot(
+      viewRegion.latitude - LOY_CENTER.latitude,
+      viewRegion.longitude - LOY_CENTER.longitude,
+    );
+    return sgwDistance < loyDistance ? Campus.SGW : Campus.Loyola;
   });
 
   useEffect(() => {
-    let sgwDistance = Math.hypot(viewRegion.latitude - SGW_CENTER.latitude, viewRegion.longitude - SGW_CENTER.longitude);
-    let loyDistance = Math.hypot(viewRegion.latitude - LOY_CENTER.latitude, viewRegion.longitude - LOY_CENTER.longitude);
-    let newSwitchValue = sgwDistance < loyDistance ? SGW : LOY;
+    let sgwDistance = Math.hypot(
+      viewRegion.latitude - SGW_CENTER.latitude,
+      viewRegion.longitude - SGW_CENTER.longitude,
+    );
+    let loyDistance = Math.hypot(
+      viewRegion.latitude - LOY_CENTER.latitude,
+      viewRegion.longitude - LOY_CENTER.longitude,
+    );
+    let newSwitchValue = sgwDistance < loyDistance ? Campus.SGW : Campus.Loyola;
 
     if (newSwitchValue !== switchValue) {
       setSwitchValue(newSwitchValue);
@@ -51,8 +62,7 @@ export default function CampusToggle({ mapRef, viewRegion }: Readonly<Props>) {
   }, [viewRegion, switchValue]);
 
   const focusCampusOnPress = (value: number) => {
-
-    let newFocus: Coordinate = value === SGW ? SGW_CENTER : LOY_CENTER;
+    let newFocus: Coordinate = value === Campus.SGW ? SGW_CENTER : LOY_CENTER;
 
     mapRef.current?.animateToRegion({
       ...newFocus,
@@ -63,7 +73,7 @@ export default function CampusToggle({ mapRef, viewRegion }: Readonly<Props>) {
   return (
     <View style={styles.container}>
       <SwitchSelector
-        initial={switchValue === LOY ? LOY : SGW}
+        initial={switchValue === Campus.Loyola ? Campus.Loyola : Campus.SGW}
         value={switchValue}
         textColor={Colors[colorScheme].campusToggle.textColor}
         selectedColor={Colors[colorScheme].campusToggle.selectedColor}
@@ -75,8 +85,8 @@ export default function CampusToggle({ mapRef, viewRegion }: Readonly<Props>) {
         fontSize={20}
         hasPadding
         options={[
-          { label: "Loyola", value: LOY },
-          { label: "SGW", value: SGW }
+          { label: "Loyola", value: Campus.Loyola },
+          { label: "SGW", value: Campus.SGW },
         ]}
         testID="campus-toggle-selector"
         accessibilityLabel="campus-toggle-selector"
