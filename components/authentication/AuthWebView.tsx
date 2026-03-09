fe49b1072b691aa64ea497d9863cf6264aa89d43
Strange4 <83730323+Strange4@users.noreply.github.com>
2026-03-08 21:25:21 -0400

    wip

--- components/authentication/AuthWebView.tsx
+++ components/authentication/AuthWebView.tsx
@@ -1,34 +1,71 @@
 import { LoggedInData } from "@/types/authTypes";
 import { useRef } from "react";
+import { Platform } from "react-native";
 import { WebView, WebViewMessageEvent, WebViewNavigation } from "react-native-webview";
+import { ShouldStartLoadRequest } from "react-native-webview/lib/WebViewTypes";
 
 interface AuthWebViewProps {
   onLogin: (data: LoggedInData) => void;
 }
 
 export default function AuthWebView({ onLogin }: Readonly<AuthWebViewProps>) {
-  const visitedAuthScreen = useRef(false);
+  const navigationState = useRef(NavigationState.LoggedOutHub);
   const webviewRef = useRef<WebView>(null);
 
   const handleMessage = (event: WebViewMessageEvent) => {
+    console.log("Got a new message from javascript");
     const cookieString = event.nativeEvent.data;
-    const data = parseCookies(cookieString);
-    onLogin(data);
+    console.log(cookieString);
+    // navigationState.current = NavigationState.LoggedOutHub;
+    // const data = parseCookies(cookieString);
+    // onLogin(data);
   };
 
   const handleNavigationChange = (event: WebViewNavigation) => {
+    console.log(`navigation changed: ${event.url}`);
+    webviewRef.current?.injectJavaScript(getCookiesJs);
+
     if (event.url.startsWith(authScreenURL)) {
-      visitedAuthScreen.current = true;
-      return;
-    }
-    if (!visitedAuthScreen.current || event.url !== authenticationURL) {
-      // I have no idea where you are but you're not in the right spot
+      console.log("Visiting the auth screen");
+      navigationState.current = NavigationState.LoggingInHub;
       return;
     }
 
-    visitedAuthScreen.current = false;
-    webviewRef.current?.injectJavaScript(getCookiesJs);
+    if(event.url.start)
+
+    if(!event.url.startsWith(authenticationURL)) {
+      // you're not in the right spot
+      console.log("You're not in the right spot");
+      return;
+    }
+
+    // if (navigationState.current === NavigationState.LoggedInHub) {
+    //   navigationState.current = NavigationState.GettingCookie
+    //   return;
+    // }
+
+    // navigationState.current = NavigationState.LoggedInHub;
+
+
+    // if(navigationState.current === NavigationState.LoggingInHub && Platform.OS === "ios") {
+    //   // We are navigating for the first time to the hub, wait for the refresh
+    //   // only happens on ios
+    //   navigationState.current = NavigationState.GettingCookie;
+    //   return;
+    // }
+    
+    // if(navigationState.current === NavigationState.GettingCookie) {
+    //   console.log("Trying to inject javascript");
+    //   webviewRef.current?.injectJavaScript(getCookiesJs);
+    //   navigationState.current = NavigationState.LoggedInHub;
+    //   return;
+    // }
   };
+
+  const shouldStartLoading = (request: ShouldStartLoadRequest) => {
+    console.log(request.url)
+    return navigationState.current !== NavigationState.LoggedInHub;
+  }
 
   return (
     <WebView
@@ -38,6 +75,8 @@ export default function AuthWebView({ onLogin }: Readonly<AuthWebViewProps>) {
       onNavigationStateChange={handleNavigationChange}
       incognito={true} // think twice before setting this to false
       sharedCookiesEnabled={true}
+      // cacheEnabled={false}
+      // onShouldStartLoadWithRequest={shouldStartLoading}
     />
   );
 }
@@ -75,10 +114,17 @@ function parseCookies(cookieString: string) {
   return loggedInData;
 }
 
-const authenticationURL = "https://hub.concordia.ca/app-sso.html?app=true";
+const authenticationURL = "https://hub.concordia.ca/";
 const authScreenURL = "https://fas.concordia.ca";
 
 const getCookiesJs = `
 window.ReactNativeWebView.postMessage(document.cookie);
 true;
 `;
+
+enum NavigationState {
+  LoggedOutHub,
+  LoggingInHub,
+  GettingCookie,
+  LoggedInHub
+}
