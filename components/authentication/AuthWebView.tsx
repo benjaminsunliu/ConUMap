f9e664a40af12115c8cb452e82963b93781aa8f5
Strange4 <83730323+Strange4@users.noreply.github.com>
2026-03-10 21:59:21 -0400

    removed warn and make it work on ios with simple fix

--- components/authentication/AuthWebView.tsx
+++ components/authentication/AuthWebView.tsx
@@ -1,71 +1,44 @@
 import { LoggedInData } from "@/types/authTypes";
 import { useRef } from "react";
-import { Platform } from "react-native";
 import { WebView, WebViewMessageEvent, WebViewNavigation } from "react-native-webview";
-import { ShouldStartLoadRequest } from "react-native-webview/lib/WebViewTypes";
 
 interface AuthWebViewProps {
   onLogin: (data: LoggedInData) => void;
 }
 
 export default function AuthWebView({ onLogin }: Readonly<AuthWebViewProps>) {
-  const navigationState = useRef(NavigationState.LoggedOutHub);
+  const visitedAuthScreen = useRef(false);
   const webviewRef = useRef<WebView>(null);
 
   const handleMessage = (event: WebViewMessageEvent) => {
-    console.log("Got a new message from javascript");
     const cookieString = event.nativeEvent.data;
-    console.log(cookieString);
-    // navigationState.current = NavigationState.LoggedOutHub;
-    // const data = parseCookies(cookieString);
-    // onLogin(data);
+    if(!cookieString) {
+      // on ios we may have to try to the cookies twice
+      // because the implementation doesn't load it the first time
+      return;
+    }
+    const data = parseCookies(cookieString);
+    visitedAuthScreen.current = false;
+    onLogin(data);
   };
 
   const handleNavigationChange = (event: WebViewNavigation) => {
-    console.log(`navigation changed: ${event.url}`);
-    webviewRef.current?.injectJavaScript(getCookiesJs);
-
     if (event.url.startsWith(authScreenURL)) {
-      console.log("Visiting the auth screen");
-      navigationState.current = NavigationState.LoggingInHub;
+      visitedAuthScreen.current = true;
       return;
     }
 
-    if(event.url.start)
-
-    if(!event.url.startsWith(authenticationURL)) {
-      // you're not in the right spot
-      console.log("You're not in the right spot");
+    if (!visitedAuthScreen.current || event.url !== authenticationURL) {
+      // You aren't in the right spot to get the cookies
       return;
     }
-
-    // if (navigationState.current === NavigationState.LoggedInHub) {
-    //   navigationState.current = NavigationState.GettingCookie
-    //   return;
-    // }
-
-    // navigationState.current = NavigationState.LoggedInHub;
-
-
-    // if(navigationState.current === NavigationState.LoggingInHub && Platform.OS === "ios") {
-    //   // We are navigating for the first time to the hub, wait for the refresh
-    //   // only happens on ios
-    //   navigationState.current = NavigationState.GettingCookie;
-    //   return;
-    // }
-    
-    // if(navigationState.current === NavigationState.GettingCookie) {
-    //   console.log("Trying to inject javascript");
-    //   webviewRef.current?.injectJavaScript(getCookiesJs);
-    //   navigationState.current = NavigationState.LoggedInHub;
-    //   return;
-    // }
+    webviewRef.current?.injectJavaScript(getCookiesJs);
   };
 
-  const shouldStartLoading = (request: ShouldStartLoadRequest) => {
-    console.log(request.url)
-    return navigationState.current !== NavigationState.LoggedInHub;
-  }
+  const handleShouldStartLoad = (event: WebViewNavigation) => {
+    const url = event.url;
+    return url.startsWith("https://") || url.startsWith("http://")
+  };
 
   return (
     <WebView
@@ -73,10 +46,9 @@ export default function AuthWebView({ onLogin }: Readonly<AuthWebViewProps>) {
       source={{ uri: authenticationURL }}
       onMessage={handleMessage}
       onNavigationStateChange={handleNavigationChange}
-      incognito={true} // think twice before setting this to false
-      sharedCookiesEnabled={true}
-      // cacheEnabled={false}
-      // onShouldStartLoadWithRequest={shouldStartLoading}
+      incognito={true} // allows us to be able to clear the cookies when the component umounts
+      originWhitelist={["https://*", "http://*", "tp-set-custom-vars://*"]}
+      onShouldStartLoadWithRequest={handleShouldStartLoad}
     />
   );
 }
@@ -114,17 +86,10 @@ function parseCookies(cookieString: string) {
   return loggedInData;
 }
 
-const authenticationURL = "https://hub.concordia.ca/";
+const authenticationURL = "https://hub.concordia.ca/app-sso.html?app=true";
 const authScreenURL = "https://fas.concordia.ca";
 
 const getCookiesJs = `
 window.ReactNativeWebView.postMessage(document.cookie);
 true;
 `;
-
-enum NavigationState {
-  LoggedOutHub,
-  LoggingInHub,
-  GettingCookie,
-  LoggedInHub
-}
