00c6377b3867c95f5e25786b6d851ef91f465693
Vinuyan Sivakolunthu <vinuyan.sivakolunthu@dawsoncollege.qc.ca>
2026-03-28 18:02:28 -0400

    changed radius range to be able to turn it off

--- constants/campusCenters.ts
+++ constants/campusCenters.ts
@@ -9,5 +9,5 @@ export const LOY_CENTER: Coordinate = {
   latitude: 45.4578596,
   longitude: -73.6395856,
 };
-export const MIN_RADIUS_METERS = 100;
+export const MIN_RADIUS_METERS = 0;
 export const MAX_RADIUS_METERS = 1000;
