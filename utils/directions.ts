bad35349c7faa9ed2f4ce90c434edabd68a063f3
Mauricio Murillo <83730323+Strange4@users.noreply.github.com>
2026-04-05 23:22:28 -0400

    Fix last sonar (#219)
    * [REFACTOR] - switch negated if statement
    
    * [REFACTOR] - remove duplicated props
    
    * [REFACTOR] - replace nested conditional with guard clause
    
    * add back console log

--- utils/directions.ts
+++ utils/directions.ts
@@ -518,14 +518,13 @@ async function handleShuttleRouting(
   }
   // if directTransit route is available, do early check to save time
 
-  if (directTransit !== null) {
-    if (
-      preShuttlePath.totalDurationSeconds + shuttleTransitTime >=
+  if (
+    directTransit !== null &&
+    preShuttlePath.totalDurationSeconds + shuttleTransitTime >=
       directTransit.totalDurationSeconds
-    ) {
-      console.log("direct transit route is faster than pre + shuttle");
-      return [];
-    }
+  ) {
+    console.log("direct transit route is faster than pre + shuttle");
+    return [];
   } else {
     console.log("no direct transit route");
   }
