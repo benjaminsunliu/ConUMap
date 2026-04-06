ff4c277daf22cc1e3ef2f04ae0c60c278459159c
Benjamin Liu <liubenjamin8@gmail.com>
2026-04-05 21:36:15 -0400

    mobile fix

--- app/+html.tsx
+++ app/+html.tsx
@@ -10,6 +10,12 @@ export default function Root({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+
         <script
           dangerouslySetInnerHTML={{
             __html: `(function (m, a, z, e) {
